f7ccd99f06866818c703340cb4b2e802657812e1
francesco <francesco.prignoli@unibo.it>
2023-05-12 12:09:17 +0200

    Update main.m

--- matlab/main.m
+++ matlab/main.m
@@ -1,6 +1,4 @@
 %% Reproduce the timing issues in matlab
-clear Controller_mex
-
 for i = 1:1000
     out = Controller_mex();
     fprintf('Measured time = %.2f [ms]\n', out.time_span*1000);
@@ -9,3 +7,4 @@ for i = 1:1000
     end
     pause(0.1)
 end
+clear Controller_mex
